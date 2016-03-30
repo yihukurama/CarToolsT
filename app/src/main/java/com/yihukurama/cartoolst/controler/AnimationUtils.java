a57b7a8f56d9ee90a3afe867fa707f4e778cba34
Administrator <479757405@qq.com>
2016-03-31 00:43:50 +0800

    增加controler来控制动画

---
+++ app/src/main/java/com/yihukurama/cartoolst/controler/AnimationUtils.java
@@ -0,0 +1,114 @@
+package com.yihukurama.cartoolst.controler;
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+
+/**
+ * Created by Administrator on 2016/3/30 0030.
+ */
+public class AnimationUtils {
+    private static final String TAG = AnimationUtils.class.getSimpleName();
+
+    /**
+     * 从控件所在位置移动到控件的底部
+     *
+     * @return
+     */
+    public static TranslateAnimation moveToViewBottom() {
+        TranslateAnimation mHiddenAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 1.0f);
+        mHiddenAction.setDuration(500);
+        return mHiddenAction;
+    }
+
+    /**
+     * 从控件的底部移动到控件所在位置
+     *
+     * @return
+     */
+    public static TranslateAnimation moveToViewLocation() {
+        TranslateAnimation mHiddenAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        mHiddenAction.setDuration(500);
+        return mHiddenAction;
+    }
+
+    /**
+     * 水平移动动画
+     */
+    public static TranslateAnimation moveHorizon(float x1,float x2,View view,long duration,long delay) {
+            float moveL = x2 - x1;
+            /*
+                    Animation还有几个方法
+                    setFillAfter(boolean fillAfter)
+                    如果fillAfter的值为真的话，动画结束后，控件停留在执行后的状态
+                    setFillBefore(boolean fillBefore)
+                    如果fillBefore的值为真的话，动画结束后，控件停留在动画开始的状态
+                    setStartOffset(long startOffset)
+                    设置动画控件执行动画之前等待的时间
+                    setRepeatCount(int repeatCount)
+                    设置动画重复执行的次数
+             */
+        TranslateAnimation translateAnimation = new TranslateAnimation(
+                //X轴初始位置
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                //X轴移动的结束位置
+                Animation.RELATIVE_TO_SELF,0.5f,
+                //y轴开始位置
+                Animation.RELATIVE_TO_SELF,0.0f,
+                //y轴移动后的结束位置
+                Animation.RELATIVE_TO_SELF,1.5f);
+
+        //3秒完成动画
+        translateAnimation.setDuration(duration);
+
+        return translateAnimation;
+    }
+
+    /**
+     * 垂直移动动画
+     */
+    public static TranslateAnimation moveVertical(int y1,int y2,View view,long duration) {
+        float moveY = y2 - y1;
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        int x = location[0];
+        int y = location[1];
+        AnimationSet animationSet = new AnimationSet(true);
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation mShowAction = new TranslateAnimation(
+                x,
+                x,
+                y+view.getHeight(),
+                y+view.getHeight()+moveY);
+        mShowAction.setDuration(duration);
+        animationSet.setFillAfter(true);
+        animationSet.addAnimation(mShowAction);
+        view.startAnimation(animationSet);
+        return mShowAction;
+    }
+
+
+    /**
+     * 向上滑动自身高度
+     */
+    public static TranslateAnimation moveSelfHeigh(View view,long duration){
+        AnimationSet animationSet = new AnimationSet(true);
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation mShowAction = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, -1.0f);
+        mShowAction.setDuration(duration);
+        animationSet.setFillAfter(true);
+        animationSet.addAnimation(mShowAction);
+        view.startAnimation(animationSet);
+        return mShowAction;
+
+    }
+}
+
--- app/src/main/java/com/yihukurama/cartoolst/view/activity/MainActivity.java
+++ app/src/main/java/com/yihukurama/cartoolst/view/activity/MainActivity.java
@@ -2,21 +2,30 @@ package com.yihukurama.cartoolst.view.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.yihukurama.cartoolst.R;
-import com.yihukurama.cartoolst.view.activity.widget.SelectPicPopupWindow;
+import com.yihukurama.cartoolst.controler.AnimationUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    SelectPicPopupWindow menuWindow;
-    ImageView imageView;
-    private GestureDetector mGestureDetector;
+    ListView list1,list2;
+    //手指按下的点为(x1, y1)手指离开屏幕的点为(x2, y2)
+    float x1 = 0;
+    float x2 = 0;
+    float y1 = 0;
+    float y2 = 0;
+    ImageView image1;
+    ImageView image2;
+    SlidingMenu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +38,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     private void initView(){
-        imageView = (ImageView)findViewById(R.id.imageView);
-        mGestureDetector = new GestureDetector(this, new MyOnGestureListener());
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                mGestureDetector.onTouchEvent(event);
-                // 一定要返回true，不然获取不到完整的事件
-                return true;
-            }
-        });
-        menuWindow = new SelectPicPopupWindow(MainActivity.this,this);
+        image1 = (ImageView)findViewById(R.id.imageView);
+        image2 = (ImageView)findViewById(R.id.imageView2);
         initSlidMenu();
 
     }
@@ -49,115 +48,114 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     }
     private void initSlidMenu(){
-        SlidingMenu menu = new SlidingMenu(this);
+        menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT_RIGHT);
-//        menu.toggle();
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         menu.setShadowWidthRes(R.dimen.shadow_width);
-//        menu.setShadowDrawable(R.drawable.octocat);
         menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeEnabled(true);
         menu.setFadeDegree(0.35f);
-        menu.setBehindScrollScale((float) 0.5);
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-        menu.setMenu(R.layout.slidmenu_layout_left);
-        menu.setSecondaryMenu(R.layout.slidmenu_layout_left);
+        menu.setMenu(R.layout.slidmenu_left);
+        setMenuList();
+        menu.setSecondaryMenu(R.layout.slidemenu_right);
+        setSecondMenuList();
+
+
+        image1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    //当手指按下的时候
+                    x1 = event.getX();
+                    y1 = event.getY();
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    //当手指离开的时候
+                    x2 = event.getX();
+                    y2 = event.getY();
+                    if(y1 - y2 > 5) {
+
+                        Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
+                        image1.setVisibility(View.VISIBLE);
+                        image2.setVisibility(View.VISIBLE);
+                        AnimationUtils.moveVertical(0,image2.getHeight(),image1, 2000);
+                    } else if(y2 - y1 > 50) {
+                        Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
+                        image2.setVisibility(View.GONE);
+                        image1.setVisibility(View.VISIBLE);
+                        image2.setAnimation(AnimationUtils.moveToViewBottom());
+                        image1.setAnimation(AnimationUtils.moveToViewLocation());
+                    } else if(x1 - x2 > 50) {
+                        Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
+                    } else if(x2 - x1 > 50) {
+                        Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return true ;
+            }
+        });
+    }
+
+
+
+    private void setSecondMenuList() {
+        list1 = (ListView) findViewById(R.id.listview_menu_right);
+        //生成动态数组，加入数据
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+        for(int i=0;i<6;i++)
+        {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", R.drawable.aa);//图像资源的ID
+            map.put("ItemTitle", "导航");
+            listItem.add(map);
+        }
+        //生成适配器的Item和动态数组对应的元素
+        SimpleAdapter listItemAdapter = new SimpleAdapter(this,listItem,//数据源
+                R.layout.left_right_item,//ListItem的XML实现
+                //动态数组与ImageItem对应的子项
+                new String[] {"ItemImage","ItemTitle"},
+                //ImageItem的XML文件里面的一个ImageView,两个TextView ID
+                new int[] {R.id.ItemImage,R.id.ItemTitle}
+        );
+
+        //添加并且显示
+        list1.setAdapter(listItemAdapter);
+    }
+
+    private void setMenuList() {
+        list2 = (ListView) findViewById(R.id.listview_menu_left);
+        //生成动态数组，加入数据
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+        for(int i=0;i<6;i++)
+        {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", R.drawable.aa);//图像资源的ID
+            map.put("ItemTitle", "导航");
+            listItem.add(map);
+        }
+        //生成适配器的Item和动态数组对应的元素
+        SimpleAdapter listItemAdapter = new SimpleAdapter(this,listItem,//数据源
+                R.layout.left_right_item,//ListItem的XML实现
+                //动态数组与ImageItem对应的子项
+                new String[] {"ItemImage","ItemTitle"},
+                //ImageItem的XML文件里面的一个ImageView,两个TextView ID
+                new int[] {R.id.ItemImage,R.id.ItemTitle}
+        );
+
+        //添加并且显示
+        list2.setAdapter(listItemAdapter);
     }
 
 
     @Override
     public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.btn_take_photo:
-                    break;
-                case R.id.btn_pick_photo:
-                    break;
                 default:
                     break;
             }
 
 
         }
-
-    private String getActionName(int action) {
-        String name = "";
-        switch (action) {
-            case MotionEvent.ACTION_DOWN: {
-                name = "ACTION_DOWN";
-                break;
-            }
-            case MotionEvent.ACTION_MOVE: {
-                name = "ACTION_MOVE";
-                break;
-            }
-            case MotionEvent.ACTION_UP: {
-                name = "ACTION_UP";
-                break;
-            }
-            default:
-                break;
-        }
-        return name;
-    }
-    class MyOnGestureListener extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            Log.i(getClass().getName(), "onSingleTapUp-----" + getActionName(e.getAction()));
-
-            return false;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-            Log.i(getClass().getName(), "onLongPress-----" + getActionName(e.getAction()));
-
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            Log.i(getClass().getName(),
-                    "onScroll-----" + getActionName(e2.getAction()) + ",(" + e1.getX() + "," + e1.getY() + ") ,("
-                            + e2.getX() + "," + e2.getY() + ")");
-            menuWindow.showAtLocation(MainActivity.this.findViewById(R.id.mainview), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0); //设置layout在PopupWindow中显示的位置
-            return false;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            Log.i(getClass().getName(),
-                    "onFling-----" + getActionName(e2.getAction()) + ",(" + e1.getX() + "," + e1.getY() + ") ,("
-                            + e2.getX() + "," + e2.getY() + ")");
-            return false;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-            Log.i(getClass().getName(), "onShowPress-----" + getActionName(e.getAction()));
-        }
-
-        @Override
-        public boolean onDown(MotionEvent e) {
-            Log.i(getClass().getName(), "onDown-----" + getActionName(e.getAction()));
-            return false;
-        }
-
-        @Override
-        public boolean onDoubleTap(MotionEvent e) {
-            Log.i(getClass().getName(), "onDoubleTap-----" + getActionName(e.getAction()));
-            return false;
-        }
-
-        @Override
-        public boolean onDoubleTapEvent(MotionEvent e) {
-            Log.i(getClass().getName(), "onDoubleTapEvent-----" + getActionName(e.getAction()));
-            return false;
-        }
-
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
-            Log.i(getClass().getName(), "onSingleTapConfirmed-----" + getActionName(e.getAction()));
-            return false;
-        }
-    }
 
 }
